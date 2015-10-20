0f7c057af54b5d90ee47505c40ffb60d05372eb4
NobuyukiSasabe <sasabe@goodf.co.jp>
2015-10-20 21:01:41 +0900

    テスト１

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello Sasabe World!');
+    res.send('Hello Sasabe2 World!');
 });
 
 var server = app.listen(port, function () {
